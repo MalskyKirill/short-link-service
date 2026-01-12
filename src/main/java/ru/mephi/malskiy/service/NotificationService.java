16eaccdafd2e4702843329a18daef30377636a06
Kirill Malskiy <malskykirill@yandex.ru>
2026-01-12 15:42:54 +0300

    добавил сервис уведомлений

---
+++ src/main/java/ru/mephi/malskiy/service/NotificationService.java
@@ -0,0 +1,9 @@
+package ru.mephi.malskiy.service;
+
+import java.util.List;
+import java.util.UUID;
+
+public interface NotificationService {
+    void notify(UUID userId, String message);
+    List<String> pullMessage(UUID userId);
+}
---
+++ src/main/java/ru/mephi/malskiy/service/NotificationServiceImpl.java
@@ -0,0 +1,19 @@
+package ru.mephi.malskiy.service;
+
+import java.util.*;
+
+public class NotificationServiceImpl implements NotificationService{
+    private final Map<UUID, List<String>> store = new HashMap<>();
+
+    @Override
+    public void notify(UUID userId, String message) {
+        store.computeIfAbsent(userId, id -> new ArrayList<>()).add(message); // если пользователь есть в хранилище возвращает его список, если нет пустой arrayList, и добавляет сообщение
+    }
+
+    @Override
+    public List<String> pullMessage(UUID userId) {
+        List<String> list = store.getOrDefault(userId, new ArrayList<>()); // берем список сообщений пользователя
+        store.remove(userId); // удаляем пользователя из хранилища
+        return list; // возвращаем сообщения
+    }
+}
